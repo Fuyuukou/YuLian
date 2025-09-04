7a11ced69aa0aa35f191c7c72c23874c044f6939
Fuyuukou <3948598726@qq.com>
2025-09-04 14:44:19 +0800

    项目初始化

---
+++ src/main/java/com/dyx/YuLianApplication.java
@@ -0,0 +1,13 @@
+package com.dyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuLianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuLianApplication.class, args);
+    }
+
+}
