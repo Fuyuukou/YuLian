18dd588675b731808132da8a63db5afeb97affb8
Fuyuukou <3948598726@qq.com>
2025-09-09 10:52:51 +0800

    9月15日

---
+++ middleware/src/main/java/com/dyx/controller/ComfyController.java
@@ -0,0 +1,9 @@
+package com.dyx.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// Comfy UI交互
+@RestController
+public class ComfyController {
+
+}
---
+++ middleware/src/main/java/com/dyx/controller/TranslatorController.java
@@ -0,0 +1,9 @@
+package com.dyx.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// DeepSeek, nllb交互
+@RestController
+public class TranslatorController {
+
+}
